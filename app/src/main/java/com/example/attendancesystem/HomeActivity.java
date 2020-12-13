51b505024358703cbf95b8077d5b56979d4016fc
jaspreet013 <jaspreetsinghpal18@gnu.ac.in>
2020-12-13 22:15:49 +0530

    Removed Comments

--- app/src/main/java/com/example/attendancesystem/HomeActivity.java
+++ app/src/main/java/com/example/attendancesystem/HomeActivity.java
@@ -19,10 +19,8 @@ import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import androidx.appcompat.app.AppCompatActivity;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.gson.Gson;
